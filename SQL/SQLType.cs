93ed252d103cbd9de0ae40f808e3c7bff6a753f1
unknown <yrezehi@gmail.com>
2023-10-25 08:26:46 -0700

    gitignore

--- SQL/SQLType.cs
+++ SQL/SQLType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace POCOToSQL
+﻿namespace POCOToSQL.SQL
 {
     public enum SQLType
     {
