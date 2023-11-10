85d80e6167c027fc4a7cde2ef123ab872aba6b76
unknown <yrezehi@gmail.com>
2023-11-10 02:54:25 -0800

    add: SQLTableCommand

---
+++ PTSQL/SQL/Commands/SQLTableCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTSQL.SQL.Tables
+{
+    public class SQLTableCommand
+    {
+        private readonly string TableName;
+        private readonly string SchemaName;
+
+        private readonly static string DEFAULT_SCHEMA_NAME = "";
+
+        public SQLTableWriter(string tableName, string? schemaName = null) =>
+            (TableName, SchemaName) = (tableName, schemaName ?? DEFAULT_SCHEMA_NAME);
+
+        public string Build()
+        {
+            return base.ToString();
+        }
+
+    }
+}
---
+++ PTSQL/SQL/Connections/Connection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTSQL.SQL.Connections
+{
+    public class Connection
+    {
+
+        private SqlConnection SQLConnection;
+
+        private Connection(string connectionString) => 
+            new SqlConnection(connectionString);
+
+        public static Connection Instance(string connectionString) =>
+            new Connection(connectionString);
+
+        public async Task Open() => 
+            await SQLConnection.OpenAsync();
+
+        public SqlCommand CreateCommand() =>
+            SQLConnection.CreateCommand();
+
+        public void Dispose() => 
+            SQLConnection.Dispose();
+    }
+}
