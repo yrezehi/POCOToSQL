dc8f368357824261767bc46c086b118a84ba7d44
unknown <yrezehi@gmail.com>
2023-11-10 05:24:17 -0800

    synv

---
+++ PTSQL/SQL/Commands/CommandConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace PTSQL.SQL.Commands
+{
+    public static class CommandConstants
+    {
+        public static string CREATE_TABLE(string tableName) =>
+            $"CREATE TABLE [{tableName}]";
+    }
+}
--- PTSQL/SQL/Commands/SQLTableCommand.cs
+++ PTSQL/SQL/Commands/SQLTableCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PTSQL.SQL.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,15 +12,25 @@ namespace PTSQL.SQL.Tables
         private readonly string TableName;
         private readonly string SchemaName;
 
+        private readonly IEnumerable<SQLColumn> Columns;
+
         private readonly static string DEFAULT_SCHEMA_NAME = "";
 
-        public SQLTableWriter(string tableName, string? schemaName = null) =>
-            (TableName, SchemaName) = (tableName, schemaName ?? DEFAULT_SCHEMA_NAME);
+        public SQLTableWriter(IEnumerable<SQLColumn> columns, string tableName, string? schemaName = null) =>
+            (Columns, TableName, SchemaName) = (columns, tableName, schemaName ?? DEFAULT_SCHEMA_NAME);
 
         public string Build()
         {
-            return base.ToString();
+            StringBuilder command = new StringBuilder();
+
+            command.AppendLine(CommandConstants.CREATE_TABLE(TableName));
+            command.Append(" ( ");
+            command.AppendLine();
+
+            return "";
         }
+
+        private string Build
 
     }
 }
