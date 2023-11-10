2409c32fd243e574fde968e6cec36fd8f7ad07c9
unknown <yrezehi@gmail.com>
2023-11-09 21:29:34 -0800

    setup: SQLColumn

---
+++ PTSQL/SQL/SQLColumn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTSQL.SQL
+{
+    public class SQLColumn
+    {
+        private readonly string ColumnName;
+        private readonly SQLType Type;
+        // minus max-length represent max
+        private readonly int MaxLength = -1;
+        private readonly bool IsNullable = true;
+
+        public SQLColumn(string name, SQLType? type = null)
+        {
+            ColumnName = name;
+            Type = type ?? SQLType.nvarchar;
+        }
+    }
+}
