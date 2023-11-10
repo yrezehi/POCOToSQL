77e8058e9d0de39a3066af92f105f43d3bb5632a
unknown <yrezehi@gmail.com>
2023-11-10 07:09:58 -0800

    setup: ClassLoader

---
+++ PTSQL/Loader/ClassLoader.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace PTSQL.Loader
+{
+    public static class ClassLoader
+    {
+        public static CompilationUnitSyntax Load(string classPath)
+        {
+            if (!File.Exists(classPath))
+            {
+                throw new FileNotFoundException($"Class was not found for {classPath}");
+            }
+
+            return ParseClassSyntax(LoadAsText(classPath));
+        }
+
+        private static string LoadAsText(string classPath)
+        {
+            using (var streamReader = new StreamReader(classPath))
+            {
+                return streamReader.ReadToEnd();
+            }
+        }
+
+        private static CompilationUnitSyntax ParseClassSyntax(string serializedClass) =>
+            (CompilationUnitSyntax) CSharpSyntaxTree.ParseText(serializedClass).GetRoot();
+    }
+}
