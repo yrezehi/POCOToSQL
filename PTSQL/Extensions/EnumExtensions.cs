13804f189c3edb1961d058734276a092c5f26ff6
unknown <yrezehi@gmail.com>
2023-11-09 21:51:38 -0800

    add: EnumExtensions

---
+++ PTSQL/Extensions/EnumExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTSQL.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string AsString(this Enum @enum) =>
+            Enum.GetName(@enum.GetType(), @enum)!;
+    }
+}
