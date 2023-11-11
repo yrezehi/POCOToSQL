053097921ffc18a6baca4fb00c2a07f1e517083c
unknown <yrezehi@gmail.com>
2023-11-10 21:33:27 -0800

    add: AttributesLoader

---
+++ PTSQL/Loader/AttributesLoader.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTSQL.Loader
+{
+    public class AttributesLoader
+    {
+
+        public CompilationUnitSyntax GetVariables(CompilationUnitSyntax syntaxTree)
+        {
+            IEnumerable<FieldDeclarationSyntax> declarations = syntaxTree.DescendantNodes().OfType<FieldDeclarationSyntax>();
+
+            foreach (var declaration in declarations)
+            {
+
+            }
+
+            return syntaxTree;
+        }
+    }
+}
