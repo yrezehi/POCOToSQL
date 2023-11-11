3bd501cefbfad0e5f028eae994fc7aa715452146
unknown <yrezehi@gmail.com>
2023-11-11 04:29:17 -0800

    enhance: SQLColumn

--- PTSQL/SQL/SQLColumn.cs
+++ PTSQL/SQL/SQLColumn.cs
@@ -6,7 +6,10 @@ namespace PTSQL.SQL
     {
         public string PropertyName;
         public readonly string ColumnName;
-        public readonly SQLType Type;
+
+        public readonly SQLType? SType;
+        public readonly Type? Type; 
+
         public readonly int MaxLength = -1;
         public readonly bool IsNullable = true;
 
@@ -14,12 +17,26 @@ namespace PTSQL.SQL
         {
             PropertyName = name;
             ColumnName = name.ToSnakeCase();
-            Type = type ?? SQLType.nvarchar;
+            SType = type;
             MaxLength = maxLength;
         }
 
+        public SQLColumn(string name, int maxLength = -1, Type? type = null)
+        {
+            PropertyName = name;
+            ColumnName = name.ToSnakeCase();
+            Type = type;
+            MaxLength = maxLength;
+        }
+
+        public static SQLColumn Create(string name, int maxLength, Type type) =>
+            new SQLColumn(name, maxLength, type);
+
+        public static SQLColumn Create(string name, int maxLength, SQLType type) =>
+            new SQLColumn(name, maxLength, type);
+
         public string DataType =>
-            Type.AsString();
+            SType == null ? SQLTypeMapper.ToSQLType(Type!).AsString() : SType.AsString();
 
         public string Length => 
             MaxLength == -1 ? " MAX " : MaxLength.ToString();
