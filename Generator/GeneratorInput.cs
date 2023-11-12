03a26a1d20a376c48fcf9ebbd01001ecc901ad74
unknown <yrezehi@gmail.com>
2023-11-12 06:13:30 -0800

    add: GeneratorInput

---
+++ Generator/GeneratorInput.cs
@@ -0,0 +1,13 @@
+﻿namespace Entry
+{
+    public class GeneratorInput
+    {
+        public IEnumerable<string> PathClasses { get; set; }
+
+        private GeneratorInput(string[] paths) =>
+            PathClasses = paths;
+
+        public GeneratorInput GetInstanceOf(params string[] paths) =>
+            new GeneratorInput(paths);
+    }
+}
--- Generator/Program.cs
+++ Generator/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using PTSQL;
 
-var syntaxTree = Generator.GenerateTable("C:\\Users\\Administrator\\Documents\\Projects\\Hisuh\\Hisuh\\Models\\Google\\GoogleMapsImage.cs");
+var syntaxTree = Generator.GenerateTable("");
 
 Console.WriteLine("Program Finished Execution!");
