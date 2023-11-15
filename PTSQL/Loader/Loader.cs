a80884bdd9937b06d4cf7cf1df845a2591a53825
unknown <yrezehi@gmail.com>
2023-11-15 07:29:28 -0800

    add: lodaer

---
+++ PTSQL/Loader/Loader.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using PTSQL.Loader.Classes;
+using PTSQL.Loader.Projects;
+using PTSQL.Loader.Variables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTSQL.Loader
+{
+    public static class Loader
+    {
+        public static Task<MSLoader> LoadProject() => 
+            ProjectsLoader.LoadProject();
+
+        public static CompilationUnitSyntax Class(string path) =>
+            ClassLoader.Load(path);
+
+        public static IList<VariableMetadata> Variables(CompilationUnitSyntax compilationSyntaxTree) =>
+            VariablesLoader.GetVariables(compilationSyntaxTree);
+    }
+}
---
+++ PTSQL/Loader/Projects/ProjectsLoader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTSQL.Loader.Projects
+{
+    public static class ProjectsLoader
+    {
+        public static void Load()
+        {
+
+        }
+    }
+}
