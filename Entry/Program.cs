b0bcc85fac803dcfb4f41799bead37bcf9e8c817
unknown <yrezehi@gmail.com>
2024-01-03 21:36:12 -0800

    set: exclude assets

---
+++ Entry/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PTSQL;
+
+var syntaxTree = Generator.GenerateTable("C:\\Users\\Administrator\\Documents\\Projects\\Notifloud-manager\\Notifloud-manager\\Core\\Models\\User.cs");
+
+Console.WriteLine("Program Finished Execution!");
--- Generator/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using PTSQL;
-
-var syntaxTree = Generator.GenerateTable("");
-
-Console.WriteLine("Program Finished Execution!");
